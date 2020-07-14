8eaafcab9766f83e1849de5b988460e8207125c1
Yuriy Larin <blandger@gmail.com>
2020-07-14 22:20:10 +0300

    updated example

--- console/examples/simple.rs
+++ console/examples/simple.rs
@@ -3,6 +3,7 @@ use ::ftdi_library::ftdi::ftdi_context::ftdi_context;
 use ::ftdi_library::ftdi::ftdi_version_info::ftdi_version_info;
 use log::{info};
 use log4rs;
+use ftdi_library::ftdi::constants::ftdi_chip_type;
 
 fn main() -> Result<(), FtdiError> {
     // log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
@@ -12,7 +13,7 @@ fn main() -> Result<(), FtdiError> {
     }
 
     info!("booting up...");
-    let mut ftdi_context = ftdi_context::new()?;
+    let mut ftdi = ftdi_context::new()?;
     info!("ftdi context in created - OK");
 
     let version = ftdi_version_info::ftdi_get_library_version();
@@ -20,9 +21,13 @@ fn main() -> Result<(), FtdiError> {
           version.version_str, version.major, version.minor, version.micro,
           version.snapshot_str);
 
-    // ftdi_context.set_interface_type(ftdi_interface::INTERFACE_ANY);
+    ftdi.ftdi_usb_open(0x0403, 0x6001)?; // fetch ony FTDI devices
+    // ftdi.ftdi_usb_open_desc_index(0, 0, None, None, 0)?; // fetch all devices
 
-    // ftdi_context.ftdi_usb_open(0x0403, 0x6001)?; // fetch ony FTDI devices
-    ftdi_context.ftdi_usb_open_desc_index(0, 0, None, None, 0)?; // fetch all devices
+    if ftdi.r#type == ftdi_chip_type::TYPE_R {
+        let chipid= ftdi.ftdi_read_chipid()?;
+        println!("FTDI chipid = {}", chipid);
+        info!("FTDI chipid = {}", chipid);
+    }
     Ok(())
 }
